6adcb5f203bfcbb3eb03865282bb6fdd6c1e9df3
s160718005 <78777781+s160718005@users.noreply.github.com>
2021-05-17 10:30:19 +0700

    bottom menu jalan

--- app/src/main/java/com/jitusolution/projectnativefoodjournal/view/MainActivity.kt
+++ app/src/main/java/com/jitusolution/projectnativefoodjournal/view/MainActivity.kt
@@ -2,11 +2,25 @@ package com.jitusolution.projectnativefoodjournal.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.jitusolution.projectnativefoodjournal.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = Navigation.findNavController(this,R.id.hostFragment)
+        NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
+        NavigationUI.setupWithNavController(navView,navController)
+
+        bottomNav.setupWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, drawerLayout) || super.onSupportNavigateUp()
     }
 }
