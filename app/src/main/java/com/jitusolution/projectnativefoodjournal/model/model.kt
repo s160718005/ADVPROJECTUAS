ec70ce12402b7ece336ab8d951f3100c91c6f601
danielyanuar07 <danielyanuar07@gmail.com>
2021-05-18 23:28:50 +0700

    create table day

--- app/src/main/java/com/jitusolution/projectnativefoodjournal/model/model.kt
+++ app/src/main/java/com/jitusolution/projectnativefoodjournal/model/model.kt
@@ -27,6 +27,25 @@ data class User(
 ){
     @PrimaryKey(autoGenerate = true)
     var uuid:Int=0
+}
+@Entity
+data class Day(
+        @ColumnInfo(name="tanggal")
+        var tanggal: String,
+        @ColumnInfo(name="namamakanan")
+        var namamakanan: String,
+        @ColumnInfo(name="kalorimakanan")
+        var kalorimakanan:Int,
+        @ColumnInfo(name="totalkalori")
+        var totalkalori:Int,
+        @ColumnInfo(name="bmr")
+        var bmr:Double,
+        @ColumnInfo(name="caloriestarget")
+        var target:Double,
+        @ColumnInfo(name="status")
+        var status: String
 
-
+        ){
+    @PrimaryKey(autoGenerate = true)
+    var uuidday:Int=0
 }
